a985150bfeea7365bba038cfc71ee8fbc51c1586
Jesus <jesus.cassano.2006@gmail.com>
2023-02-24 19:25:23 +0100

    ✅ Add components files

---
+++ src/router/beers.router.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { BeersController } from '../controllers/beers.controller.js';
+import { BeersFileRepo } from '../repository/beers.file.repo.js';
+
+// eslint-disable-next-line new-cap
+export const beersRouter = Router();
+const repo = new BeersFileRepo();
+const controller = new BeersController(repo);
+
+beersRouter.get('/', controller.getAll.bind(controller));
+beersRouter.get('/:id', controller.get.bind(controller));
+beersRouter.post('/', controller.post.bind(controller));
+beersRouter.patch('/:id', controller.patch.bind(controller));
+beersRouter.delete('/:id', controller.delete.bind(controller));
