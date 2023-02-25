a23e6df9413272b4315e3295c8ae6f3e6414dff8
Jesus <jesus.cassano.2006@gmail.com>
2023-02-25 16:41:03 +0100

    ✅ Add  fix changes

--- src/app.ts
+++ src/app.ts
@@ -2,31 +2,15 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import { beersRouter } from './router/beers.router.js';
+
 export const app = express();
 app.disable('x-powered-by');
-
 const corsOptions = {
   origin: '*',
 };
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors(corsOptions));
 
-// Modo más organizado de hacerlo
-// Ejemplo para una ruta
-
 app.use('/beers', beersRouter);
-
-// Modo más simple de hacerlo
-// Ejemplo para la ruta home
-
-app.get('/:id', (req, resp) => {
-  resp.send('Hola ' + req.params.id);
-});
-app.post('/', (req, resp) => {
-  req.body.id = 12;
-  resp.send(req.body);
-});
-
-app.patch('/:id');
-app.delete('/:id');
