eeee8d16f8435ff45ea589e80ee9143dcc79ab8c
Jesus <jesus.cassano.2006@gmail.com>
2023-02-25 16:58:45 +0100

    ✅ Add files changes beersFile

--- src/repository/beers.file.repo.ts
+++ src/repository/beers.file.repo.ts
@@ -1,15 +1,54 @@
+/* eslint-disable lines-between-class-members */
 import fs from 'fs/promises';
 
 const file = './data/data.json';
+
+export type Beers = {
+  id: number;
+  name: string;
+  price: number;
+};
+
+export interface BeersRepoStructure {
+  read(): Promise<Beers[]>;
+  // eslint-disable-next-line no-unused-vars
+  write(info: Beers): Promise<string | object>;
+}
 
 export class BeersFileRepo {
   read() {
     return fs
       .readFile(file, { encoding: 'utf-8' })
-      .then((data) => JSON.parse(data) as any[]);
+      .then((data) => JSON.parse(data) as Beers[]);
   }
 
-  write() {}
-  update() {}
-  delete() {}
+  async readId(id: Beers['id']) {
+    const data = await fs.readFile(file, 'utf-8');
+    const parsedData: Beers[] = JSON.parse(data);
+    return parsedData.filter((thing) => thing.id === id)[0];
+  }
+
+  async write(info: Beers) {
+    const data = await fs.readFile(file, { encoding: 'utf-8' });
+    const parsedData: Beers[] = JSON.parse(data);
+    const finalData = JSON.stringify([...parsedData, info]);
+    await fs.writeFile(file, finalData, { encoding: 'utf-8' });
+    return 'Lets go! (write)';
+  }
+  async update(info: Beers) {
+    const data = await fs.readFile(file, { encoding: 'utf-8' });
+    const parsedData: Beers[] = JSON.parse(data);
+    const updatedData = JSON.stringify(
+      parsedData.map((item) => (item.id === info.id ? info : item))
+    );
+    await fs.writeFile(file, updatedData, 'utf-8');
+  }
+  async delete(id: Beers['id']) {
+    const data = await fs.readFile(file, 'utf-8');
+    const parsedData: Beers[] = JSON.parse(data);
+    const finalData = JSON.stringify(
+      parsedData.filter((item) => item.id !== id)
+    );
+    await fs.writeFile(file, finalData, 'utf-8');
+  }
 }
