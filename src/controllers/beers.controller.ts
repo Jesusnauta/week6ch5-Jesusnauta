fd7dd295bdcfd1a22b4f1a91bf5c9bb859563f86
Jesus <jesus.cassano.2006@gmail.com>
2023-02-26 13:27:33 +0100

    ✅ Add beers controller file

--- src/controllers/beers.controller.ts
+++ src/controllers/beers.controller.ts
@@ -1,23 +1,50 @@
+/* eslint-disable prefer-destructuring */
 import { Response, Request } from 'express';
-import { BeersFileRepo } from '../repository/beers.file.repo.js';
+import { Beers, BeersFileRepo } from '../repository/beers.file.repo.js';
 
 export class BeersController {
   // eslint-disable-next-line no-useless-constructor, no-unused-vars
   constructor(public repo: BeersFileRepo) {}
 
-  getAll(_req: Request, resp: Response) {
+  getAll(req: Request, resp: Response) {
     this.repo.read().then((data) => {
       resp.json(data);
     });
   }
 
   get(req: Request, resp: Response) {
-    resp.send('This is a beer ' + req.params.id);
+    this.repo.read().then((data) => {
+      console.log(data);
+      const id = req.params.id;
+      const cerveza = data.find((item) => item.id === Number(id));
+      resp.json(cerveza);
+    });
   }
 
-  post(_req: Request, _resp: Response) {}
+  post(req: Request, resp: Response) {
+    console.log(req.body);
+    this.repo.write(req.body).then((data) => console.log(data));
+    resp.send('<p> Post! </p>');
+  }
 
-  patch(_req: Request, _resp: Response) {}
+  async patch(req: Request, resp: Response) {
+    const {
+      params: { id },
+    } = req;
+    if (!id) {
+      return;
+    }
 
-  delete(_req: Request, _resp: Response) {}
+    const updateInfo = req.body as Partial<Beers>;
+    const dataToUpdate = await this.repo.readId(Number(req.params.id));
+    const updatedBeers = Object.assign(dataToUpdate, updateInfo);
+    console.log(updatedBeers);
+    await this.repo.update(updatedBeers);
+    resp.send(`<p>Only updated a ' + ${id}</p>`);
+  }
+
+  async delete(req: Request, resp: Response) {
+    await this.repo.delete(Number(req.params.id));
+    resp.send(`<p>Deleted item ${req.params.id}`);
+  }
 }
