0af47b9936d8736a8885ca9e381237e0a3c18f96
Jesus <jesus.cassano.2006@gmail.com>
2023-02-27 20:46:35 +0100

    ✅ Add beers controller changes

--- src/controllers/beers.controller.ts
+++ src/controllers/beers.controller.ts
@@ -1,50 +1,64 @@
-import { Response, Request } from 'express';
-import { BeersFileRepo } from '../repository/beers.file.repo.js';
+import { Response, Request, NextFunction } from 'express';
+import { Repo } from '../repository/repo.interface.js';
+import { Beer } from '../entities/beers';
 
 export class BeersController {
   // eslint-disable-next-line no-useless-constructor, no-unused-vars
-  constructor(public repo: BeersFileRepo) {}
+  constructor(public repo: Repo<Beer>) {}
 
-  getAll(_req: Request, resp: Response) {
-    this.repo.read().then((data) => {
-      resp.json(data);
-    });
-  }
-
-  get(req: Request, resp: Response) {
-    this.repo.read().then((data: any[]) => {
-      console.log(data);
-      const { id } = req.params;
-      const specificData = data.find(
-        (item: { id: number }) => item.id === Number(id)
-      );
-
-      resp.json(specificData);
-    });
-  }
-
-  post(req: Request, resp: Response) {
-    console.log(req.body);
-    this.repo.write(req.body).then((data: any) => console.log(data));
-    resp.send(`<p> Post </p>`);
-  }
-
-  async patch(req: Request, resp: Response) {
-    const id = Number(req.params.id);
-    if (!id) {
-      return;
+  async getAll(_req: Request, resp: Response, next: NextFunction) {
+    try {
+      const data = await this.repo.query();
+      resp.json({
+        results: data,
+      });
+    } catch (error) {
+      next(error);
     }
-
-    const prevBeer: any = await this.repo.readId(id);
-    console.log(prevBeer);
-    const newBeer = req.body;
-    const updateBeer = Object.assign(prevBeer, newBeer);
-    await this.repo.update(updateBeer);
-    resp.send('Updated');
   }
 
-  async delete(req: Request, resp: Response) {
-    await this.repo.delete(Number(req.params.id));
-    resp.send(`<p>Deleted  ${req.params.id}`);
+  async get(req: Request, resp: Response, next: NextFunction) {
+    try {
+      const data = await this.repo.queryId(req.params.id);
+      resp.json({
+        results: [data],
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async post(req: Request, resp: Response, next: NextFunction) {
+    try {
+      const data = await this.repo.create(req.body);
+      resp.json({
+        results: [data],
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async patch(req: Request, resp: Response, next: NextFunction) {
+    try {
+      req.body.id = req.params.id ? req.params.id : req.body.id;
+      const data = await this.repo.update(req.body);
+      resp.json({
+        results: [data],
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async delete(req: Request, resp: Response, next: NextFunction) {
+    try {
+      this.repo.destroy(req.params.id);
+      resp.json({
+        results: [],
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 }
